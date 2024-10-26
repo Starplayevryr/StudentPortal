63b639c2d7a2d6c31ae04be8ea8a8cd952c02d9a
Keerthana R <148705349+Starplayevryr@users.noreply.github.com>
2024-10-26 11:57:29 +0530

    Update app.component.ts

--- StudentFrontend/src/app/app.component.ts
+++ StudentFrontend/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { DarkModeService } from './dark-mode.service'; // Adjust the path as needed
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  title = 'StudentFrontend';
+  apiUrl = 'https://student-portal-jade-pi.vercel.app'; // Your Vercel API URL
   /*
   title = 'StudentFrontend';
   constructor(private darkModeService: DarkModeService) {}
